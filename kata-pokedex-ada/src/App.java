bc3d272ba9cd7390a39f3658c51aa6c9cb39fef8
driss292 <drisskaci@outlook.fr>
2024-10-25 10:59:55 +0200

    Etape 1 - Attrapez les tous !

--- kata-pokedex-ada/src/App.java
+++ kata-pokedex-ada/src/App.java
@@ -1,5 +1,20 @@
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.*;
+import java.io.FileReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("test Kata");
+        try {
+
+            Object pokemon = new JSONParser().parse(new FileReader("pokemon.json"));
+            JSONObject pokemonToJson = (JSONObject) pokemon;
+            JSONArray pokemonArray = (JSONArray) pokemonToJson.get("pokemon");
+
+            System.out.println(pokemonArray);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
